4c366060dd9fea0c5277b5db3a90fa498a5f97df
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-10-05 15:58:56 +0300

    findAllimp added

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,7 +1,12 @@
 package ma.hibernate.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -36,6 +41,17 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder criteria = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> phoneCriteriaQuery = criteria.createQuery(Phone.class);
+            Root<Phone> root = phoneCriteriaQuery.from(Phone.class);
+            List<Predicate> predicates = new ArrayList<>();
+            for (Map.Entry<String, String[]> entry : params.entrySet()) {
+                String parameter = entry.getKey();
+                predicates.add(root.get(entry.getKey()).in(entry.getValue()));
+            }
+            phoneCriteriaQuery.select(root).where(predicates.toArray(new Predicate[]{}));
+            return session.createQuery(phoneCriteriaQuery).getResultList();
+        }
     }
 }
