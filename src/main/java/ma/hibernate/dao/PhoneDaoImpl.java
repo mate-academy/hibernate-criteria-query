29e1847c4aee60e2b8e6863f20efb5db307fcfda
Borisko130 <krimenyukb@gmail.com>
2020-10-05 17:50:50 +0300

    Passes all tests

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -2,12 +2,16 @@ package ma.hibernate.dao;
 
 import java.util.List;
 import java.util.Map;
-
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.exception.DataProcessingException;
 import ma.hibernate.model.Phone;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.tool.hbm2ddl.SingleLineSqlCommandExtractor;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -28,7 +32,7 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't insert Content entity", e);
+            throw new DataProcessingException("Can't insert Phone entity", e);
         } finally {
             if (session != null) {
                 session.close();
@@ -38,6 +42,19 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> phoneCriteriaQuery = criteriaBuilder.createQuery(Phone.class);
+            Root<Phone> phoneRoot = phoneCriteriaQuery.from(Phone.class);
+            Predicate predicate = criteriaBuilder.conjunction();
+            for (Map.Entry<String, String[]> entry : params.entrySet()) {
+                predicate = criteriaBuilder.and(predicate,
+                        phoneRoot.get(entry.getKey()).in(entry.getValue()));
+            }
+            phoneCriteriaQuery.select(phoneRoot).where(predicate);
+            return session.createQuery(phoneCriteriaQuery).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Failed to find phones by parameters", e);
+        }
     }
 }
