df073dabf2031221bac83f8712111e1f26f3f547
romanHreits <romagreic@gmail.com>
2020-10-05 16:34:14 +0300

    fixed checkStyle

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -3,8 +3,10 @@ package ma.hibernate.dao;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import javax.persistence.criteria.*;
-
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.exception.DataProcessingException;
 import ma.hibernate.model.Phone;
 import org.hibernate.Session;
