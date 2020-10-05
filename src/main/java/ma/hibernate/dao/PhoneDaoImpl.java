1d82810cb0b9cad3f7f7e2c5d270359d438ff397
Pavlo Khomiak <pavlo.khomiak93@gmail.com>
2020-10-05 17:31:21 +0300

    Add solution

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,9 +1,18 @@
 package ma.hibernate.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +21,53 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(phone);
+            transaction.commit();
+            return phone;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't create phone", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        List<Phone> list = null;
+        Session session = factory.openSession();
+        try {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> q = cb.createQuery(Phone.class);
+            Root<Phone> root = q.from(Phone.class);
+            List<Predicate> andPredicate = new ArrayList<>();
+            Predicate finalPredicate = null;
+            for (String key : params.keySet()) {
+                List<Predicate> orPredicate = new ArrayList<>();
+                for (String value : params.get(key)) {
+                    Path<Object> objectPath = root.get(key);
+                    orPredicate.add(cb.equal(objectPath, value));
+                }
+                Predicate oneCriterionCombineOrPredicate =
+                        cb.or(orPredicate.toArray(new Predicate[]{}));
+                andPredicate.add(oneCriterionCombineOrPredicate);
+            }
+            finalPredicate = cb.and(andPredicate.toArray(new Predicate[]{}));
+            Query<Phone> query = session.createQuery(q
+                    .where(finalPredicate));
+            list = query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find phones", e);
+        }
+        return list;
     }
 }
