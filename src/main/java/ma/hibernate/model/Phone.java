1e7dc294aa9d19aa0cefefd595349f02c799f639
Artem Vremere <vremere_a@ukr.net>
2020-10-05 17:02:55 +0300

    lombok add from Phone class

--- src/main/java/ma/hibernate/model/Phone.java
+++ src/main/java/ma/hibernate/model/Phone.java
@@ -1,11 +1,10 @@
 package ma.hibernate.model;
 
-import lombok.Data;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Data;
 
 @Data
 @Entity
