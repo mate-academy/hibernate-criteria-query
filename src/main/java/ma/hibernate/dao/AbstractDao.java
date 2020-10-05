01f9c29c43adf4f5baeca411ff720861bcb55c3a
jlightgreen <ShvabovichJulia@gmail.com>
2020-10-05 22:02:15 +0300

    hb-03

--- src/main/java/ma/hibernate/dao/AbstractDao.java
+++ src/main/java/ma/hibernate/dao/AbstractDao.java
@@ -1,9 +1,32 @@
 package ma.hibernate.dao;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
-public abstract class AbstractDao {
+public abstract class AbstractDao<T> {
     protected final SessionFactory factory;
+
+    T create(T entity) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+            return entity;
+        } catch (Exception exception) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Couldn't create entity", exception);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
 
     protected AbstractDao(SessionFactory sessionFactory) {
         this.factory = sessionFactory;
--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,22 +1,42 @@
 package ma.hibernate.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
-public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
+public class PhoneDaoImpl extends AbstractDao<Phone> implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        return super.create(phone);
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> query = builder.createQuery(Phone.class);
+            Root<Phone> root = query.from(Phone.class);
+            List<Predicate> predicates = new ArrayList<>();
+            for (Map.Entry<String, String[]> entry : params.entrySet()) {
+                predicates.add(root.get(entry.getKey())
+                        .in((Object[]) entry.getValue()));
+            }
+            query.select(root).where(predicates.toArray(new Predicate[]{}));
+            return session.createQuery(query).getResultList();
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Couldn't get list of phones");
+        }
     }
 }
