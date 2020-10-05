d7c9dd47aa18cf2611ade8209b835f7a2ce2425c
Artem Vremere <vremere_a@ukr.net>
2020-10-05 13:48:29 +0300

    add DataProcessingException impl create method

---
+++ src/main/java/ma/hibernate/exeptions/DataProcessingException.java
@@ -0,0 +1,7 @@
+package ma.hibernate.exeptions;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
