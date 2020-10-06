d75f30b035f5b7657fae0eb1f585d59dd98de7ce
Andriy <andriy.lagun@gmail.com>
2020-10-06 08:33:38 +0300

    reformatted imports after mvn clean package

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -2,11 +2,11 @@ package ma.hibernate.dao;
 
 import java.util.List;
 import java.util.Map;
-import ma.hibernate.model.Phone;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import ma.hibernate.model.Phone;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
