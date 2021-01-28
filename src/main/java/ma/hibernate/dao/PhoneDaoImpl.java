c59d5bb1857b6f62105d00e1588bbcdad080d057
Max Pochepets <agreed_note@hotmail.com>
2021-01-28 14:28:42 +0200

    Fix:
    dynamic parameters idea applied - findAll made more agile

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,6 +1,5 @@
 package ma.hibernate.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import javax.persistence.criteria.CriteriaBuilder;
@@ -45,56 +44,16 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
             CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
             CriteriaQuery<Phone> getAllPhonesQuery = criteriaBuilder.createQuery(Phone.class);
             Root<Phone> root = getAllPhonesQuery.from(Phone.class);
-            if (params.size() != 0) {
-                List<Predicate> parameters = new ArrayList<>();
-                if (params.get("model") != null) {
-                    Predicate[] modelParameters = new Predicate[params.get("model").length];
-                    for (int i = 0; i < modelParameters.length; i++) {
-                        modelParameters[i] = criteriaBuilder
-                                .equal(root.get("model"), params.get("model")[i]);
-                    }
-                    parameters.add(criteriaBuilder.or(modelParameters));
+            Predicate predicate = criteriaBuilder.and();
+            for (Map.Entry<String, String[]> entry : params.entrySet()) {
+                CriteriaBuilder.In<String> parametersRange
+                        = criteriaBuilder.in(root.get(entry.getKey()));
+                for (String parameter : entry.getValue()) {
+                    parametersRange.value(parameter);
                 }
-                if (params.get("maker") != null) {
-                    Predicate[] makerParameters = new Predicate[params.get("maker").length];
-                    for (int i = 0; i < makerParameters.length; i++) {
-                        makerParameters[i] = criteriaBuilder
-                                .equal(root.get("maker"), params.get("maker")[i]);
-                    }
-                    parameters.add(criteriaBuilder.or(makerParameters));
-                }
-                if (params.get("color") != null) {
-                    Predicate[] colorParameters = new Predicate[params.get("color").length];
-                    for (int i = 0; i < colorParameters.length; i++) {
-                        colorParameters[i] = criteriaBuilder
-                                .equal(root.get("color"), params.get("color")[i]);
-                    }
-                    parameters.add(criteriaBuilder.or(colorParameters));
-                }
-                if (params.get("os") != null) {
-                    Predicate[] osParameters = new Predicate[params.get("os").length];
-                    for (int i = 0; i < osParameters.length; i++) {
-                        osParameters[i] = criteriaBuilder
-                                .equal(root.get("os"), params.get("os")[i]);
-                    }
-                    parameters.add(criteriaBuilder.or(osParameters));
-                }
-                if (params.get("countryManufactured") != null) {
-                    Predicate[] countryManufacturedParameters
-                            = new Predicate[params.get("countryManufactured").length];
-                    for (int i = 0; i < countryManufacturedParameters.length; i++) {
-                        countryManufacturedParameters[i] = criteriaBuilder
-                                .equal(root.get("countryManufactured"),
-                                        params.get("countryManufactured")[i]);
-                    }
-                    parameters.add(criteriaBuilder.or(countryManufacturedParameters));
-                }
-                Predicate selectPredicate = criteriaBuilder
-                        .and(parameters.toArray(Predicate[]::new));
-                getAllPhonesQuery.select(root).where(selectPredicate);
-            } else {
-                getAllPhonesQuery.select(root);
+                predicate = criteriaBuilder.and(predicate, parametersRange);
             }
+            getAllPhonesQuery.select(root).where(predicate);
             return session.createQuery(getAllPhonesQuery).getResultList();
         } catch (Exception e) {
             throw new RuntimeException("Could not get a list of phones from DB. ", e);
