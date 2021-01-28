028250ee38edd4b6189be467cc951c52874d382a
Alex <65593125+Artificial-Friend@users.noreply.github.com>
2021-01-28 17:08:28 +0200

    sol

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,9 +1,16 @@
 package ma.hibernate.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +19,50 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(phone);
+            transaction.commit();
+            return phone;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("error: can't add phone = " + phone, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> query = criteriaBuilder.createQuery(Phone.class);
+            Root<Phone> root = query.from(Phone.class);
+            //root.in(array); IGNORE THIS COMMENT PLEASE. I want to try optimize it later,now solvd
+            List<Predicate> andPredicates = new ArrayList<>();
+            for (Map.Entry<String, String[]> entry : params.entrySet()) {
+                String[] values = entry.getValue();
+                List<Predicate> orPredicates = new ArrayList<>();
+                for (String value : values) {
+                    Predicate orPredicate = criteriaBuilder.equal(root.get(entry.getKey()), value);
+                    orPredicates.add(orPredicate);
+                }
+                Predicate andPredicate = criteriaBuilder.or(
+                        orPredicates.toArray(new Predicate[]{}));
+                andPredicates.add(andPredicate);
+            }
+            CriteriaQuery<Phone> where = query.select(root).where(
+                    andPredicates.toArray(new Predicate[0]));
+            return session.createQuery(where).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("can't find all phones with params " + params, e);
+        }
     }
 }
