41deda6645838723a54c8c742b3fc8874fb7e492
Darina <to.darina.to>
2021-06-07 15:21:58 +0300

    all done

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,9 +1,16 @@
 package ma.hibernate.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +19,45 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(phone);
+            transaction.commit();
+            return phone;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't create phone to DB");
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            List<CriteriaBuilder.In<String>> arrayList = new ArrayList<>();
+
+            CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
+            Root<Phone> productRoot = query.from(Phone.class);
+
+            for (Map.Entry<String, String[]> stringEntry : params.entrySet()) {
+                CriteriaBuilder.In<String> field =
+                        cb.in(productRoot.get(stringEntry.getKey()));
+                for (String param : stringEntry.getValue()) {
+                    field.value(param);
+                }
+                arrayList.add(field);
+            }
+            query.where(cb.and(arrayList.toArray(new Predicate[0])));
+            return session.createQuery(query).getResultList();
+        }
     }
 }
