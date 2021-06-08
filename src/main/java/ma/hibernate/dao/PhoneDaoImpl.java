df8c6eaa41f65b5710383ca96f25d6d320826c7f
Alina <vasylyshyn.alina@gmail.com>
2021-06-08 12:13:05 +0300

    added catch block

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -41,19 +41,21 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
         try (Session session = factory.openSession()) {
-            CriteriaBuilder cb = session.getCriteriaBuilder();
-            CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
-            Root<Phone> phoneRoot = query.from(Phone.class);
-            Predicate predicate = cb.and();
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> findAllPhoneQuery = criteriaBuilder.createQuery(Phone.class);
+            Root<Phone> phoneRoot = findAllPhoneQuery.from(Phone.class);
+            Predicate predicate = criteriaBuilder.and();
             for (Map.Entry<String, String[]> entry : params.entrySet()) {
-                CriteriaBuilder.In<String> model = cb.in(phoneRoot.get(entry.getKey()));
+                CriteriaBuilder.In<String> model = criteriaBuilder.in(phoneRoot.get(entry.getKey()));
                 for (String value : entry.getValue()) {
                     model.value(value);
                 }
-                predicate = cb.and(predicate, model);
+                predicate = criteriaBuilder.and(predicate, model);
             }
-            query.where(predicate);
-            return session.createQuery(query).getResultList();
+            findAllPhoneQuery.where(predicate);
+            return session.createQuery(findAllPhoneQuery).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find all phone ", e);
         }
     }
 }
