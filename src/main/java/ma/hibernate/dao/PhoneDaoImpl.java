ce9d0559d1695b8ec077192519fc2880e1de26a9
Andrii Romaniuk <romaniukandrii7@gmail.com>
2021-06-08 06:23:11 +0300

    first solution

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -2,8 +2,14 @@ package ma.hibernate.dao;
 
 import java.util.List;
 import java.util.Map;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +18,48 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(phone);
+            transaction.commit();
+            return phone;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Exception during creation of phone: " + phone, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> findByParamsQuery = cb.createQuery(Phone.class);
+            Root<Phone> phoneRoot = findByParamsQuery.from(Phone.class);
+            Predicate allPredicates = cb.and();
+            for (Map.Entry<String, String[]> entry : params.entrySet()) {
+                CriteriaBuilder.In<String> newInPredicate =
+                        cb.in(phoneRoot.get(entry.getKey()));
+                for (String data : entry.getValue()) {
+                    newInPredicate.value(data);
+                }
+                allPredicates = cb.and(allPredicates, newInPredicate);
+            }
+            findByParamsQuery.where(allPredicates);
+            return session.createQuery(findByParamsQuery).getResultList();
+        } catch (Exception e) {
+            System.out.println("Exception while trying to get phones from db "
+                    + "by parameters:");
+            params.forEach((key, value) -> System.out.println(key + " : " + value));
+            throw new RuntimeException("Stack trace: ", e);
+        }
     }
 }
