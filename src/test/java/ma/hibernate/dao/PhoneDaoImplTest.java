31cfeb4c36dfee3c861b12dfea94d1633781fd93
Nikitozin <nikitozin91@gmail.com>
2021-08-04 20:15:38 +0300

    CheckStyle

--- src/test/java/ma/hibernate/dao/PhoneDaoImplTest.java
+++ src/test/java/ma/hibernate/dao/PhoneDaoImplTest.java
@@ -3,7 +3,6 @@ package ma.hibernate.dao;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import ma.hibernate.model.Phone;
 import org.junit.Assert;
 import org.junit.Before;
