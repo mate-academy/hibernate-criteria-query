30700ebad8e875ac7c07a27b57b5d55aac4bcd92
mishamolnar <michaellmuller423@gmail.com>
2021-09-30 08:00:44 +0300

    common mistakes check

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -2,14 +2,14 @@ package ma.hibernate.dao;
 
 import java.util.List;
 import java.util.Map;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -30,7 +30,7 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Can't insert movie " + phone, e);
+            throw new RuntimeException("Can't create phone " + phone, e);
         } finally {
             if (session != null) {
                 session.close();
@@ -44,7 +44,17 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
             CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
             CriteriaQuery<Phone> query = criteriaBuilder.createQuery(Phone.class);
             Root<Phone> phoneRoot = query.from(Phone.class);
-            
+            Predicate mainPredicate = criteriaBuilder.isTrue(phoneRoot.isNotNull());
+            for (Map.Entry<String, String[]> parameters : params.entrySet()) {
+                CriteriaBuilder.In<String> parameterPredicate =
+                        criteriaBuilder.in(phoneRoot.get(parameters.getKey()));
+                for (String parametr : parameters.getValue()) {
+                    parameterPredicate.value(parametr);
+                }
+                mainPredicate = criteriaBuilder.and(mainPredicate, parameterPredicate);
+            }
+            query.where(mainPredicate);
+            return session.createQuery(query).getResultList();
         }
     }
 }
