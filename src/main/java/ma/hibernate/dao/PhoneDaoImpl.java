666c8030c77b91c1bb0f9e21ec0c319e921cca95
Andrii Demchenko <avd998ne@gmail.com>
2021-10-27 17:46:22 +0300

    added task solution, removed redundant cfg file

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -2,6 +2,10 @@ package ma.hibernate.dao;
 
 import java.util.List;
 import java.util.Map;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -37,6 +41,23 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> query = criteriaBuilder.createQuery(Phone.class);
+            Root<Phone> phoneRoot = query.from(Phone.class);
+            Predicate mapPredicate = criteriaBuilder.and();
+            for (Map.Entry<String, String[]> param : params.entrySet()) {
+                CriteriaBuilder.In<String> inPredicate = criteriaBuilder
+                        .in(phoneRoot.get(param.getKey()));
+                for (String string : param.getValue()) {
+                    inPredicate.value(string);
+                }
+                mapPredicate = criteriaBuilder.and(mapPredicate, inPredicate);
+            }
+            query.where(mapPredicate);
+            return session.createQuery(query).list();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find all devices with given params map.", e);
+        }
     }
 }
