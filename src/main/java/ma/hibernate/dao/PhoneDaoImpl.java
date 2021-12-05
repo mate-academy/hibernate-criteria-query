5c1853fd84673ad6fe0c34674f20a258b106f1bd
Vladimir Nedashkovskiy <vo-v4ik@hotmail.com>
2021-12-05 20:15:18 +0200

    implemented method findAll

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,5 +1,9 @@
 package ma.hibernate.dao;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
@@ -36,6 +40,22 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = this.factory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
+            Root<Phone> phoneRoot = query.from(Phone.class);
+            Predicate predicate = cb.and();
+            for (Map.Entry<String, String[]> el : params.entrySet()) {
+                CriteriaBuilder.In<Object> predicatePar = cb.in(phoneRoot.get(el.getKey()));
+                for (String value : el.getValue()) {
+                    predicatePar.value(value);
+                }
+                predicate = cb.and(predicate, predicatePar);
+            }
+            query.where(predicate);
+            return session.createQuery(query).getResultList();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Can't get phones with input parameters", e);
+        }
     }
 }
