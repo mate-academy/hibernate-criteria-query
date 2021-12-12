7e45a2d1014a5102d8d0694d3f5f1c18f0d3b2c1
Dima Petrushenko <dmytro.petrushenko@gmail.com>
2021-12-12 21:23:42 +0200

    create done, findAll in proceed

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -2,8 +2,17 @@ package ma.hibernate.dao;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +21,62 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(phone);
+            transaction.commit();
+            return phone;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert a phone " + phone
+                    + "in a DB!", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
+            Root<Phone> phoneRoot = query.from(Phone.class);
+            Set<Map.Entry<String, String[]>> entrySet = params.entrySet();
+
+            CriteriaBuilder.In<Object> countryManufacturedPredicate = null;
+            CriteriaBuilder.In<Object> makerPredicate = null;
+            CriteriaBuilder.In<Object> colorPredicate = null;
+
+            for (Map.Entry<String, String[]> parameter : entrySet) {
+                if (Objects.equals(parameter.getKey(), "countryManufactured")) {
+                    countryManufacturedPredicate = cb.in(phoneRoot
+                            .get("countryManufactured"));
+                    for (String country : parameter.getValue()) {
+                        countryManufacturedPredicate.value(country);
+                    }
+                }
+                if (Objects.equals(parameter.getKey(), "maker")) {
+                    makerPredicate = cb.in(phoneRoot.get("maker"));
+                    for (String maker : parameter.getValue()) {
+                        makerPredicate.value(maker);
+                    }
+                }
+                if (Objects.equals(parameter.getKey(), "color")) {
+                    colorPredicate = cb.in(phoneRoot.get("color"));
+                    for (String color : parameter.getValue()) {
+                        colorPredicate.value(color);
+                    }
+                }
+            }
+            query.where(cb.and(countryManufacturedPredicate, makerPredicate, colorPredicate));
+            return session.createQuery(query).getResultList();
+        }
     }
 }
