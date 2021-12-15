b13e645b9a9513a5c2eee0fa9e1ae6a240aec0a1
DmytroPetrushenko <dmytro.petrushenko@gmail.com>
2021-12-15 11:28:00 +0200

    techers notice fixed. mvn ok

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 import java.util.Set;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
 import org.hibernate.Session;
@@ -47,71 +48,30 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
             CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
             Root<Phone> phoneRoot = query.from(Phone.class);
             Set<Map.Entry<String, String[]>> entrySet = params.entrySet();
-
-            CriteriaBuilder.In<Object> countryManufacturedPredicate = null;
-            CriteriaBuilder.In<Object> makerPredicate = null;
-            CriteriaBuilder.In<Object> colorPredicate = null;
-            CriteriaBuilder.In<Object> modelPredicate = null;
-
+            Predicate predicateMaker = cb.conjunction();
+            Predicate predicateModel = cb.conjunction();
+            Predicate predicateCountryManufactured = cb.conjunction();
+            Predicate predicateColor = cb.conjunction();
             for (Map.Entry<String, String[]> parameter : entrySet) {
-                if (Objects.equals(parameter.getKey(), "countryManufactured")) {
-                    countryManufacturedPredicate = cb.in(phoneRoot
-                            .get("countryManufactured"));
-                    for (String country : parameter.getValue()) {
-                        countryManufacturedPredicate.value(country);
-                    }
-                }
                 if (Objects.equals(parameter.getKey(), "maker")) {
-                    makerPredicate = cb.in(phoneRoot.get("maker"));
-                    for (String maker : parameter.getValue()) {
-                        makerPredicate.value(maker);
-                    }
-                }
-                if (Objects.equals(parameter.getKey(), "color")) {
-                    colorPredicate = cb.in(phoneRoot.get("color"));
-                    for (String color : parameter.getValue()) {
-                        colorPredicate.value(color);
-                    }
+                    predicateMaker = cb.and(predicateMaker, phoneRoot.get(parameter.getKey())
+                            .in(parameter.getValue()));
                 }
                 if (Objects.equals(parameter.getKey(), "model")) {
-                    modelPredicate = cb.in(phoneRoot.get("model"));
-                    for (String model : parameter.getValue()) {
-                        modelPredicate.value(model);
-                    }
+                    predicateModel = cb.and(predicateModel, phoneRoot.get(parameter.getKey())
+                            .in(parameter.getValue()));
+                }
+                if (Objects.equals(parameter.getKey(), "countryManufactured")) {
+                    predicateCountryManufactured = cb.and(predicateCountryManufactured, phoneRoot
+                            .get(parameter.getKey()).in(parameter.getValue()));
+                }
+                if (Objects.equals(parameter.getKey(), "color")) {
+                    predicateColor = cb.and(predicateColor, phoneRoot.get(parameter.getKey())
+                            .in(parameter.getValue()));
                 }
             }
-
-            if (countryManufacturedPredicate != null && makerPredicate != null
-                    && colorPredicate != null) {
-                query.where(cb.and(countryManufacturedPredicate, makerPredicate, colorPredicate));
-            }
-            if (countryManufacturedPredicate != null && makerPredicate != null
-                    && colorPredicate == null) {
-                query.where(cb.and(countryManufacturedPredicate, makerPredicate));
-            }
-            if (countryManufacturedPredicate != null && makerPredicate == null
-                    && colorPredicate == null) {
-                query.where(cb.and(countryManufacturedPredicate));
-            }
-            if (countryManufacturedPredicate == null && makerPredicate != null
-                    && colorPredicate != null) {
-                query.where(cb.and(makerPredicate, colorPredicate));
-            }
-            if (countryManufacturedPredicate == null && makerPredicate != null
-                    && colorPredicate == null) {
-                query.where(cb.and(makerPredicate));
-            }
-            if (countryManufacturedPredicate == null && makerPredicate == null
-                    && colorPredicate != null) {
-                query.where(cb.and(colorPredicate));
-            }
-            if (countryManufacturedPredicate != null && makerPredicate == null
-                    && colorPredicate != null) {
-                query.where(cb.and(countryManufacturedPredicate, colorPredicate));
-            }
-            if (modelPredicate != null) {
-                query.where(cb.and(modelPredicate));
-            }
+            query.where(cb.and(predicateMaker, predicateModel, predicateCountryManufactured,
+                    predicateColor));
             return session.createQuery(query).getResultList();
         }
     }
