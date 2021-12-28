1262d7af5094f20f2f545f4f41ba15ee123a609b
Vladyslav Chyver <lione2009lione@gmail.com>
2021-12-28 13:22:13 +0200

    code was optimized

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,9 +1,7 @@
 package ma.hibernate.dao;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
@@ -47,15 +45,15 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
             CriteriaBuilder cb = session.getCriteriaBuilder();
             CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
             Root<Phone> root = query.from(Phone.class);
-            Set<CriteriaBuilder.In<String>> inClauses = new HashSet<>();
+            Predicate conjunctionOfClauses = cb.conjunction();
             for (Map.Entry<String, String[]> entry : params.entrySet()) {
                 CriteriaBuilder.In<String> inClause = cb.in(root.get(entry.getKey()));
                 for (String option : entry.getValue()) {
                     inClause.value(option);
                 }
-                inClauses.add(inClause);
+                conjunctionOfClauses = cb.and(conjunctionOfClauses, inClause);
             }
-            query.where(cb.and(inClauses.toArray(new Predicate[0])));
+            query.where(conjunctionOfClauses);
             return session.createQuery(query).getResultList();
         } catch (HibernateException exception) {
             throw new RuntimeException("Couldn't find all phones with params: "
