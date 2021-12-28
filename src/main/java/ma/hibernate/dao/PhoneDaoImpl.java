870f3dc69b96f3360589b0100beebb4f4fbd789c
Vladyslav Chyver <lione2009lione@gmail.com>
2021-12-28 13:53:19 +0200

    code was optimized after review

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,7 +1,9 @@
 package ma.hibernate.dao;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
@@ -45,32 +47,21 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
             CriteriaBuilder cb = session.getCriteriaBuilder();
             CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
             Root<Phone> root = query.from(Phone.class);
-            Predicate conjunctionOfClauses = cb.conjunction();
+            Predicate inPredicate = cb.conjunction();
             for (Map.Entry<String, String[]> entry : params.entrySet()) {
                 CriteriaBuilder.In<String> inClause = cb.in(root.get(entry.getKey()));
                 for (String option : entry.getValue()) {
                     inClause.value(option);
                 }
-                conjunctionOfClauses = cb.and(conjunctionOfClauses, inClause);
+                inPredicate = cb.and(inPredicate, inClause);
             }
-            query.where(conjunctionOfClauses);
+            query.where(inPredicate);
             return session.createQuery(query).getResultList();
         } catch (HibernateException exception) {
             throw new RuntimeException("Couldn't find all phones with params: "
-                    + convertToString(params));
+                    + params.entrySet().stream()
+                    .map(v -> "(" + v.getKey() + Arrays.toString(v.getValue()) + ")")
+                    .collect(Collectors.joining("; ")));
         }
-    }
-
-    private String convertToString(Map<String, String[]> params) {
-        StringBuilder paramsInString = new StringBuilder();
-        for (Map.Entry<String, String[]> entry : params.entrySet()) {
-            paramsInString.append("( ").append(entry.getKey())
-                    .append(" ");
-            for (String value : entry.getValue()) {
-                paramsInString.append(value).append(", ");
-            }
-            paramsInString.append(")");
-        }
-        return paramsInString.toString();
     }
 }
