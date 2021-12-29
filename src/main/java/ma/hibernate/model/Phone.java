eef7317228a576a015186295530a7359dd509cd6
Admin Oleksandr <maslo5@meta.ua>
2021-12-29 11:38:58 +0200

    fixed mistakes

--- src/main/java/ma/hibernate/model/Phone.java
+++ src/main/java/ma/hibernate/model/Phone.java
@@ -4,10 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "phones")
 public class Phone implements Cloneable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
