ed0fc81f9ef486c6dd055ade8652be0f39611dcd
ASelivonchyk <aselivonchyk@gmail.com>
2022-01-17 22:33:49 +0200

    Wrote an assignment hibernate-criteria-query. - implemented methods create() and findAll() according to the assigment.

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,9 +1,16 @@
 package ma.hibernate.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +19,44 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(phone);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save phone: " + phone + " to Database", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return phone;
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> findAllQuery = cb.createQuery(Phone.class);
+            Root<Phone> root = findAllQuery.from(Phone.class);
+            List<Predicate> predicates = new ArrayList<>(params.size());
+            for (Map.Entry<String, String[]> entry : params.entrySet()) {
+                CriteriaBuilder.In<String> predicate = cb.in(root.get(entry.getKey()));
+                for (String value : entry.getValue()) {
+                    predicate.value(value);
+                }
+                predicates.add(predicate);
+            }
+            findAllQuery.where(cb.and(predicates.toArray(Predicate[]::new)));
+            return session.createQuery(findAllQuery).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get phones from Database ", e);
+        }
     }
 }
