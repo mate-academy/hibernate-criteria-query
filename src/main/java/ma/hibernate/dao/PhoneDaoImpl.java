ae63553b439bc4e74cbc623bb06feeb8830b7a8c
AlexPravda1 <kabinkos@gmail.com>
2022-02-21 11:08:18 +0400

    -implemented multiple params as predicate

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,7 +1,12 @@
 package ma.hibernate.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -25,7 +30,7 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Can't insert phone to DB.", e);
+            throw new RuntimeException("Can't insert phone to DB." + phone, e);
         } finally {
             if (session != null) {
                 session.close();
@@ -36,6 +41,52 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> phoneQuery = cb.createQuery(Phone.class);
+            Root<Phone> phoneRoot = phoneQuery.from(Phone.class);
+
+            CriteriaBuilder.In<String> modelPredicate = cb.in(phoneRoot.get("model"));
+            CriteriaBuilder.In<String> makerPredicate = cb.in(phoneRoot.get("maker"));
+            CriteriaBuilder.In<String> colorPredicate = cb.in(phoneRoot.get("color"));
+            CriteriaBuilder.In<String> osPredicate = cb.in(phoneRoot.get("os"));
+            CriteriaBuilder.In<String> countryPredicate
+                    = cb.in(phoneRoot.get("countryManufactured"));
+
+            List<Predicate> conditions = new ArrayList<>();
+
+            for (Map.Entry<String, String[]> param : params.entrySet()) {
+                String[] paramsArray = param.getValue();
+                for (String paramCurrentValue : paramsArray) {
+                    switch (param.getKey()) {
+                        case "model":
+                            modelPredicate.value(paramCurrentValue);
+                            conditions.add(modelPredicate);
+                            break;
+                        case "maker":
+                            makerPredicate.value(paramCurrentValue);
+                            conditions.add(makerPredicate);
+                            break;
+                        case "color":
+                            colorPredicate.value(paramCurrentValue);
+                            conditions.add(colorPredicate);
+                            break;
+                        case "os":
+                            osPredicate.value(paramCurrentValue);
+                            conditions.add(osPredicate);
+                            break;
+                        case "countryManufactured":
+                            countryPredicate.value(paramCurrentValue);
+                            conditions.add(countryPredicate);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            return session.createQuery(phoneQuery.select(phoneRoot)
+                 .where(conditions.toArray(new Predicate[] {})))
+                 .getResultList();
+        }
     }
 }
