6e0b7b3ba7ed7e0a4bf1af25677731438c599997
AprioriKS <sounddummies@gmail.com>
2022-04-10 12:14:17 +0300

    finished

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -2,8 +2,13 @@ package ma.hibernate.dao;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaBuilder.In;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
-import ma.hibernate.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -18,7 +23,7 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
         Transaction transaction = null;
         Session session = null;
         try {
-            session = HibernateUtil.getSessionFactory().openSession();
+            session  = factory.openSession();
             transaction = session.beginTransaction();
             session.save(phone);
             transaction.commit();
@@ -37,6 +42,25 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session  = factory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
+            Root<Phone> phoneRoot = query.from(Phone.class);
+
+            Predicate mainPredicate = cb.and();
+            for (Entry<String, String[]> entry : params.entrySet()) {
+                In<Object> in = cb.in(phoneRoot.get(entry.getKey()));
+                for (String values : entry.getValue()) {
+                    in.value(values);
+                }
+                mainPredicate = cb.and(mainPredicate, in);
+            }
+
+            query.where(mainPredicate);
+            return session.createQuery(query).getResultList();
+
+
+
+        }
     }
 }
