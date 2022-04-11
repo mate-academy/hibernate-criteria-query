cf79818f64a9ecc1815bc8f2219bac179b368962
Olexsii_Titov <drfrankinsheyn16@gmail.com>
2022-04-11 12:32:15 +0300

    PhoneDao.findAll() was implemented

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -7,6 +7,11 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
@@ -36,6 +41,23 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> criteriaQuery = criteriaBuilder.createQuery(Phone.class);
+            Root<Phone> phoneRoot = criteriaQuery.from(Phone.class);
+            Predicate predicate = criteriaBuilder.and();
+            CriteriaBuilder.In<Object> paramPredicate;
+            for (Map.Entry<String, String[]> stringEntry : params.entrySet()) {
+                paramPredicate = criteriaBuilder.in(phoneRoot.get(stringEntry.getKey()));
+                for (String s : stringEntry.getValue()) {
+                    paramPredicate.value(s);
+                }
+                predicate = criteriaBuilder.and(predicate, paramPredicate);
+            }
+            criteriaQuery.where(predicate);
+            return session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find a phone by this params: " + params);
+        }
     }
 }
