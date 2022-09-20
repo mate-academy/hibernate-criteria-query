941710206e8cdb815b87ec1a22af894daa662cc3
Nazar <litvik.12@gmail.com>
2022-09-20 09:37:49 +0300

    added solution with using for each

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -50,41 +50,15 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
             Root<Phone> phoneRoot = query.from(Phone.class);
 
             ArrayList<Predicate> predicates = new ArrayList<>();
-            CriteriaBuilder.In<String> makerPredicate = cb.in(phoneRoot.get("maker"));
-            params.entrySet().stream().filter(p -> p.getKey().equals("maker"))
-                    .map(Map.Entry::getValue)
-                    .flatMap(Arrays::stream)
-                    .forEach(makerPredicate::value);
-            if (((InPredicate)makerPredicate).getValues().size() > 0) {
-                predicates.add(makerPredicate);
-            }
-
-            CriteriaBuilder.In<String> countryPredicate =
-                    cb.in(phoneRoot.get("countryManufactured"));
-            params.entrySet().stream().filter(p -> p.getKey().equals("countryManufactured"))
-                    .map(Map.Entry::getValue)
-                    .flatMap(Arrays::stream)
-                    .forEach(countryPredicate::value);
-            if (((InPredicate)countryPredicate).getValues().size() > 0) {
-                predicates.add(countryPredicate);
-            }
-
-            CriteriaBuilder.In<String> modelPredicate = cb.in(phoneRoot.get("model"));
-            params.entrySet().stream().filter(p -> p.getKey().equals("model"))
-                    .map(Map.Entry::getValue)
-                    .flatMap(Arrays::stream)
-                    .forEach(modelPredicate::value);
-            if (((InPredicate)modelPredicate).getValues().size() > 0) {
-                predicates.add(modelPredicate);
-            }
-
-            CriteriaBuilder.In<String> colorPredicate = cb.in(phoneRoot.get("color"));
-            params.entrySet().stream().filter(p -> p.getKey().equals("color"))
-                    .map(Map.Entry::getValue)
-                    .flatMap(Arrays::stream)
-                    .forEach(colorPredicate::value);
-            if (((InPredicate)colorPredicate).getValues().size() > 0) {
-                predicates.add(colorPredicate);
+            for (Map.Entry<String, String[]> param : params.entrySet()) {
+                CriteriaBuilder.In<Object> inPredicate = cb.in(phoneRoot.get(param.getKey()));
+                params.entrySet().stream()
+                        .map(Map.Entry::getValue)
+                        .flatMap(Arrays::stream)
+                        .forEach(inPredicate::value);
+                if (((InPredicate)inPredicate).getValues().size() > 0) {
+                    predicates.add(inPredicate);
+                }
             }
 
             query.where(cb.and(predicates.toArray(new Predicate[0])));
