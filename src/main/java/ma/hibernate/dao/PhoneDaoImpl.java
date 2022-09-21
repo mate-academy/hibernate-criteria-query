43f1d47a1ad56c530b531a31025b52ae39a8357f
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-09-21 16:06:48 +0300

    Homework is done

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -2,8 +2,14 @@ package ma.hibernate.dao;
 
 import java.util.List;
 import java.util.Map;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +18,45 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(phone);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't create the phone: " + phone, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return phone;
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery findAllWithFilteringQuery = criteriaBuilder.createQuery(Phone.class);
+            Root<Phone> phoneRoot = findAllWithFilteringQuery.from(Phone.class);
+            Predicate paramsAnd = criteriaBuilder.conjunction();
+            for (Map.Entry entry: params.entrySet()) {
+                CriteriaBuilder.In<String> paramsIn =
+                        criteriaBuilder.in(phoneRoot.get((String) entry.getKey()));
+                for (String value: (String[]) entry.getValue()) {
+                    paramsIn.value(value);
+                }
+                paramsAnd = criteriaBuilder.and(paramsAnd, paramsIn);
+            }
+            findAllWithFilteringQuery.where(paramsAnd);
+            return session.createQuery(findAllWithFilteringQuery).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find all Phones with params: " + params, e);
+        }
     }
 }
