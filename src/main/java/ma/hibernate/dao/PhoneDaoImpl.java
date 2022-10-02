f2a28f01376eff38a4db4637b2b3971de84722b3
Mykola Skrypka <mykolaskrypka@gmail.com>
2022-10-02 12:29:16 +0300

    implemented all PhoneDao methods; method findAll() implemented with CriteriaQuery

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -3,7 +3,14 @@ package ma.hibernate.dao;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +19,50 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(phone);
+            transaction.commit();
+            return phone;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert phone " + phone, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        Session session = factory.openSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
+        Root<Phone> rootPhone = query.from(Phone.class);
+        Predicate predicate = null;
+        for (Map.Entry<String, String[]> param : params.entrySet()) {
+            if (param.getValue().length == 0) {
+                continue;
+            }
+            CriteriaBuilder.In<String> paramPredicate = cb.in(rootPhone.get(param.getKey()));
+            for (String value : param.getValue()) {
+                paramPredicate.value(value);
+            }
+            if (predicate == null) {
+                predicate = cb.and(paramPredicate);
+            } else {
+                predicate = cb.and(predicate, paramPredicate);
+            }
+        }
+        if (predicate != null) {
+            query.where(predicate);
+        }
+        return session.createQuery(query).getResultList();
     }
 }
