ea80183539c473e5831f6c5f91e91ee8377027b8
Igor Petrichenko <i.petrichenko@gmail.com>
2022-10-23 13:51:59 +0300

    HW Hibernate Criteria Query, fixed

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,20 +1,16 @@
 package ma.hibernate.dao;
 
-import java.security.KeyStore;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-
-import ma.hibernate.model.Phone;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import ma.hibernate.model.Phone;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -50,9 +46,10 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
             CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
             Root<Phone> phoneRoot = query.from(Phone.class);
             List<Predicate> predicateList = new ArrayList<>();
-            for(Map.Entry<String, String[]> key: params.entrySet()) {
-                CriteriaBuilder.In<String> keyPredicate = cb.in(phoneRoot.get(key.getKey()));
-                for(String value: key.getValue()) {
+            for (Map.Entry<String, String[]> key: params.entrySet()) {
+                CriteriaBuilder.In<String> keyPredicate = cb
+                        .in(phoneRoot.get(key.getKey()));
+                for (String value: key.getValue()) {
                     keyPredicate.value(value);
                 }
                 predicateList.add(keyPredicate);
