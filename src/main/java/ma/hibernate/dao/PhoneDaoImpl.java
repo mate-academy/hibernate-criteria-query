945b3c58a750d4036c696974e405dc2aedfc0c64
Fokin <Fokin1@ukr.net>
2022-12-07 20:49:55 +0100

    implement findAll method in PhoneDaoImpl class

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,7 +1,12 @@
 package ma.hibernate.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -34,6 +39,19 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = super.factory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> criteriaBuilderQuery = criteriaBuilder.createQuery(Phone.class);
+            Root<Phone> from = criteriaBuilderQuery.from(Phone.class);
+            List<Predicate> predicates = new ArrayList<>();
+            for (Map.Entry<String, String[]> entry : params.entrySet()) {
+                CriteriaBuilder.In<Object> predicate = criteriaBuilder.in(from.get(entry.getKey()));
+                for (String value : entry.getValue()) {
+                    predicates.add(predicate.value(value));
+                }
+            }
+            criteriaBuilderQuery.where(predicates.toArray(new Predicate[]{}));
+            return session.createQuery(criteriaBuilderQuery).getResultList();
+        }
     }
 }
