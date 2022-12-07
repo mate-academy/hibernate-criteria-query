5fedb66de20bfbc189b0ff737d52fb46b268575b
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2022-12-07 16:46:48 +0200

    tests are passed

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,15 +1,16 @@
 package ma.hibernate.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -44,19 +45,18 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
             CriteriaBuilder cb = session.getCriteriaBuilder();
             CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
             Root<Phone> phoneRoot = query.from(Phone.class);
-            // where key in 'value1, value2, value3...'
+            List<Predicate> predicateList = new ArrayList<>();
             for (Map.Entry<String, String[]> entry : params.entrySet()) {
                 String param = entry.getKey();
                 String[] values = entry.getValue();
                 CriteriaBuilder.In<String> paramPredicate = cb.in(phoneRoot.get(param));
                 for (String value : values) {
                     paramPredicate.value(value);
-//                    query.where(cb.equal(phoneRoot.get(entry.getKey()), value));
                 }
-                cb.and(paramPredicate);
+                predicateList.add(paramPredicate);
             }
-            List<Phone> resultList = session.createQuery(query).getResultList();
-            return resultList;
+            query.where(cb.and(predicateList.toArray(new Predicate[0])));
+            return session.createQuery(query).getResultList();
         } catch (Exception e) {
             throw new RuntimeException("Can't find all by parameters: " + params, e);
         }
