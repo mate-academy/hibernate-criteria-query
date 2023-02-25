41f8ec2c6fadb6b220c9961188cc29d7eff9f8a2
Kateryna Takhtai <ktakhtay22@gmail.com>
2023-02-25 17:26:38 +0100

    style changes for criteria-query

--- src/main/java/ma/hibernate/exception/DataProcessingException.java
+++ src/main/java/ma/hibernate/exception/DataProcessingException.java
@@ -1,6 +1,6 @@
 package ma.hibernate.exception;
 
-public class DataProcessingException extends RuntimeException{
+public class DataProcessingException extends RuntimeException {
     public DataProcessingException(String message, Throwable cause) {
         super(message, cause);
     }
