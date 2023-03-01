5ac24c254f92d9579a56b566b5524a3e74a341a2
Oleksandr Kravchenko <oleksandr.kravchenko@spirent.com>
2023-03-01 23:46:25 +0200

    feat: hibernate-criteria-query #comment was used creteriaQuery

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -2,8 +2,14 @@ package ma.hibernate.dao;
 
 import java.util.List;
 import java.util.Map;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +18,55 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(phone);
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+
+            throw new RuntimeException("Can not save Phone to DB. Phone: " + phone, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return phone;
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> query = criteriaBuilder.createQuery(Phone.class);
+            Root<Phone> rootClass = query.from(Phone.class);
+
+            Predicate[] predicateList = new Predicate[params.size()];
+            int count = 0;
+
+            for (Map.Entry<String, String[]> record: params.entrySet()) {
+                CriteriaBuilder.In<String> inPredicate =
+                        criteriaBuilder.in(rootClass.get(record.getKey()));
+                for (String value : record.getValue()) {
+                    inPredicate.value(value);
+                }
+                predicateList[count++] = inPredicate;
+            }
+
+            query.where(predicateList);
+
+            return session.createQuery(query).getResultList();
+        } catch (Exception e) {
+
+            throw new RuntimeException("Can not get list of Phones by params. Params: "
+                    + params, e);
+        }
     }
 }
