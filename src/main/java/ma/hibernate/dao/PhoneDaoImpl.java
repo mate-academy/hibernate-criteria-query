08eb7fa7a4488491d5679a0aa401c10900572755
Andrii Malenko <andmalenko@gmail.com>
2023-03-23 11:46:53 +0200

    Here is my solution for hw-criteria-query

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -2,8 +2,14 @@ package ma.hibernate.dao;
 
 import java.util.List;
 import java.util.Map;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +18,47 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(phone);
+            transaction.commit();
+        } catch (Exception e) {
+            if (session != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't create new phone in DB " + phone, e);
+        } finally {
+            if (transaction != null) {
+                session.close();
+            }
+        }
+        return phone;
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> phoneCriteriaQuery = cb.createQuery(Phone.class);
+            Root<Phone> phoneRoot = phoneCriteriaQuery.from(Phone.class);
+            Predicate[] cbList = new Predicate[params.size()];
+            int i = 0;
+            for (String mapKey : params.keySet()) {
+                CriteriaBuilder.In<String> keyPredicate = cb.in(phoneRoot.get(mapKey));
+                for (String param : params.get(mapKey)) {
+                    keyPredicate.value(param);
+                }
+                cbList[i] = keyPredicate;
+                i++;
+            }
+            Predicate phonePredicate = cb.and(cbList);
+            phoneCriteriaQuery.where(phonePredicate);
+            return session.createQuery(phoneCriteriaQuery).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all phones from DB", e);
+        }
     }
 }
