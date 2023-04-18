7b56f3fa72fc5087d76a74628e1e0f89d97c2925
Oleksii Stepaniak <alexstepanyak@gmail.com>
2023-04-19 01:14:31 +0300

    all imports were fixed

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,9 +1,7 @@
 package ma.hibernate.dao;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
