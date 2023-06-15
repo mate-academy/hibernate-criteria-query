57c53079ecb87889eea4310e3ef6e65c540ca805
Dmytro Katrevych <katrevichdv@gmail.com>
2023-06-15 13:50:43 +0300

    PhoneDao methods were implemented.

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -2,7 +2,12 @@ package ma.hibernate.dao;
 
 import java.util.List;
 import java.util.Map;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
@@ -12,11 +17,27 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        try (Session session = factory.openSession()) {
+            session.save(phone);
+        } catch (Exception e) {
+            throw new RuntimeException("Can`t save phone: " + phone, e);
+        }
+        return phone;
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
+            Root<Phone> root = query.from(Phone.class);
+            Predicate[] predicates = params.entrySet().stream()
+                    .map(entry -> root.get(entry.getKey()).in((Object[]) entry.getValue()))
+                    .toArray(Predicate[]::new);
+            query.where(predicates);
+            return session.createQuery(query).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can`t get phones list with params: " + params, e);
+        }
     }
 }
