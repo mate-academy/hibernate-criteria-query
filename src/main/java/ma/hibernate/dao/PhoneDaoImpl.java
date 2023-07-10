b1dba3728d141a1a5968ffeea6b70bdcd8628ab0
Mykyta Naumov <nneed4use@gmail.com>
2023-07-10 11:26:28 +0300

    First commit

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,9 +1,18 @@
 package ma.hibernate.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +21,46 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(phone);
+            transaction.commit();
+            return phone;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert phone " + phone, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+
+        try {
+            Session session = factory.openSession();
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
+            Root<Phone> from = query.from(Phone.class);
+            Set<Map.Entry<String, String[]>> entrySet = params.entrySet();
+            List<Predicate> predicates = new ArrayList<>();
+            for (Map.Entry<String, String[]> entry : entrySet) {
+                String field = entry.getKey();
+                String[] values = entry.getValue();
+                predicates.add(from.get(field).in((Object[]) values));
+            }
+            query.select(from).where(predicates.toArray(new Predicate[0]));
+            return session.createQuery(query).getResultList();
+
+        } catch (HibernateException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
