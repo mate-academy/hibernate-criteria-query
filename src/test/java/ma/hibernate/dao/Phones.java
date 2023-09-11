d5b057309a21217558b6b4aee36631cfab1dce24
Yuriy Kononchuk <y_kononchuk@ukr.net>
2023-09-11 16:21:40 +0300

    adeed solution for Hibernate_Criteria_Query

--- src/test/java/ma/hibernate/dao/Phones.java
+++ src/test/java/ma/hibernate/dao/Phones.java
@@ -3,70 +3,70 @@ package ma.hibernate.dao;
 import ma.hibernate.model.Phone;
 
 public class Phones {
-    public static final Phone iphoneX = new Phone();
-    public static final Phone iphone7 = new Phone();
-    public static final Phone samsungA5 = new Phone();
-    public static final Phone samsungA7White = new Phone();
-    public static final Phone samsungA7Red = new Phone();
-    public static final Phone samsungA7Black = new Phone();
-    public static final Phone oppo10white = new Phone();
-    public static final Phone xiaomiRedmi5 = new Phone();
+    public static final Phone IPHONE_X = new Phone();
+    public static final Phone IPHONE_7 = new Phone();
+    public static final Phone SAMSUNG_A5 = new Phone();
+    public static final Phone SAMSUNG_A7_WHITE = new Phone();
+    public static final Phone SAMSUNG_A7_RED = new Phone();
+    public static final Phone SAMSUNG_A7_BLACK = new Phone();
+    public static final Phone OPPO_10_WHITE = new Phone();
+    public static final Phone XIAOMI_REDMI_5 = new Phone();
 
     static {
         // iPhoneX
-        iphoneX.setModel("iPhone X");
-        iphoneX.setColor("red");
-        iphoneX.setMaker("Apple");
-        iphoneX.setOs("iOS");
-        iphoneX.setCountryManufactured("USA");
+        IPHONE_X.setModel("iPhone X");
+        IPHONE_X.setColor("red");
+        IPHONE_X.setMaker("Apple");
+        IPHONE_X.setOs("iOS");
+        IPHONE_X.setCountryManufactured("USA");
 
         // iPhone7
-        iphone7.setModel("iPhone 7");
-        iphone7.setColor("white");
-        iphone7.setMaker("Apple");
-        iphone7.setOs("iOS");
-        iphone7.setCountryManufactured("USA");
+        IPHONE_7.setModel("iPhone 7");
+        IPHONE_7.setColor("white");
+        IPHONE_7.setMaker("Apple");
+        IPHONE_7.setOs("iOS");
+        IPHONE_7.setCountryManufactured("USA");
 
         // samsungA5
-        samsungA5.setModel("A5");
-        samsungA5.setColor("white");
-        samsungA5.setMaker("Samsung");
-        samsungA5.setOs("Android");
-        samsungA5.setCountryManufactured("Korea");
+        SAMSUNG_A5.setModel("A5");
+        SAMSUNG_A5.setColor("white");
+        SAMSUNG_A5.setMaker("Samsung");
+        SAMSUNG_A5.setOs("Android");
+        SAMSUNG_A5.setCountryManufactured("Korea");
 
         // samsung A7 white
-        samsungA7White.setModel("A7");
-        samsungA7White.setColor("white");
-        samsungA7White.setMaker("Samsung");
-        samsungA7White.setOs("Android");
-        samsungA7White.setCountryManufactured("Korea");
+        SAMSUNG_A7_WHITE.setModel("A7");
+        SAMSUNG_A7_WHITE.setColor("white");
+        SAMSUNG_A7_WHITE.setMaker("Samsung");
+        SAMSUNG_A7_WHITE.setOs("Android");
+        SAMSUNG_A7_WHITE.setCountryManufactured("Korea");
 
         // samsung A7 red
-        samsungA7Red.setModel("A7");
-        samsungA7Red.setColor("red");
-        samsungA7Red.setMaker("Samsung");
-        samsungA7Red.setOs("Android");
-        samsungA7Red.setCountryManufactured("Korea");
+        SAMSUNG_A7_RED.setModel("A7");
+        SAMSUNG_A7_RED.setColor("red");
+        SAMSUNG_A7_RED.setMaker("Samsung");
+        SAMSUNG_A7_RED.setOs("Android");
+        SAMSUNG_A7_RED.setCountryManufactured("Korea");
 
         // samsung A7 black
-        samsungA7Black.setModel("A7");
-        samsungA7Black.setColor("red");
-        samsungA7Black.setMaker("Samsung");
-        samsungA7Black.setOs("Android");
-        samsungA7Black.setCountryManufactured("Korea");
+        SAMSUNG_A7_BLACK.setModel("A7");
+        SAMSUNG_A7_BLACK.setColor("red");
+        SAMSUNG_A7_BLACK.setMaker("Samsung");
+        SAMSUNG_A7_BLACK.setOs("Android");
+        SAMSUNG_A7_BLACK.setCountryManufactured("Korea");
 
         // Oppo 10 white
-        oppo10white.setModel("Oppo10");
-        oppo10white.setColor("white");
-        oppo10white.setMaker("Oppo");
-        oppo10white.setOs("Android");
-        oppo10white.setCountryManufactured("China");
+        OPPO_10_WHITE.setModel("Oppo10");
+        OPPO_10_WHITE.setColor("white");
+        OPPO_10_WHITE.setMaker("Oppo");
+        OPPO_10_WHITE.setOs("Android");
+        OPPO_10_WHITE.setCountryManufactured("China");
 
         // Xiaomi Redmi 5
-        xiaomiRedmi5.setModel("Redmi 5");
-        xiaomiRedmi5.setColor("black");
-        xiaomiRedmi5.setMaker("Xiaomi");
-        xiaomiRedmi5.setOs("Android");
-        xiaomiRedmi5.setCountryManufactured("China");
+        XIAOMI_REDMI_5.setModel("Redmi 5");
+        XIAOMI_REDMI_5.setColor("black");
+        XIAOMI_REDMI_5.setMaker("Xiaomi");
+        XIAOMI_REDMI_5.setOs("Android");
+        XIAOMI_REDMI_5.setCountryManufactured("China");
     }
 }
