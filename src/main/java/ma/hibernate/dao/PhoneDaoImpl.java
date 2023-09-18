77ea11f4d0766c7b137ef1365658b2286c22f690
yuriistorozhuk <yurii.storozhuk@gmail.com>
2023-09-18 19:13:17 +0300

    second variant

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -4,6 +4,7 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
@@ -41,6 +42,44 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        Transaction transaction = null;
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> query = criteriaBuilder.createQuery(Phone.class);
+            Root<Phone> root = query.from(Phone.class);
+            List<Predicate> predicate = new ArrayList<>();
+            String[] countryManufacturedValues = params.get("countryManufactured");
+            String[] producerValues = params.get("maker");
+            String[] colorValues = params.get("color");
+            String[] modelValues = params.get("model");
+            predicate.add(
+                    countryManufacturedValues != null && countryManufacturedValues.length > 0
+                            ? root.get("countryManufactured")
+                            .in((Object[]) countryManufacturedValues)
+                            : criteriaBuilder.and()
+            );
+            predicate.add(
+                    producerValues != null && producerValues.length > 0
+                            ? root.get("maker").in((Object[]) producerValues)
+                            : criteriaBuilder.and()
+            );
+            predicate.add(
+                    colorValues != null && colorValues.length > 0
+                            ? root.get("color").in((Object[]) colorValues)
+                            : criteriaBuilder.and()
+            );
+            predicate.add(
+                    modelValues != null && modelValues.length > 0
+                            ? root.get("model").in((Object[]) modelValues)
+                            : criteriaBuilder.and()
+            );
+            Predicate finalPredicate = criteriaBuilder
+                    .and(predicate.toArray(predicate.toArray(new Predicate[0])));
+            query.where(finalPredicate);
+            return session.createQuery(query).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Error while finding phones", e);
+        }
     }
 }
