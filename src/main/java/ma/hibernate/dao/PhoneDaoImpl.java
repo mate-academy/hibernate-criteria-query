9bbb8e7e8f35f5c49c90420a03b3d3395207becb
Olena <yelenastankevych@gmail.com>
2023-09-29 01:14:44 +0300

    Implemented findAll method

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,8 +1,15 @@
 package ma.hibernate.dao;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
@@ -12,11 +19,30 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        try {
+            factory.inTransaction(s -> s.persist(phone));
+            return phone;
+        } catch (Exception e) {
+            throw new RuntimeException("Can't create phone: " + phone, e);
+        }
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
+            Root<Phone> phoneRoot = query.from(Phone.class);
+            List<Predicate> phonePredicate = new ArrayList<>();
+            params.forEach((key, value) -> {
+                CriteriaBuilder.In<String> predicate = cb.in(phoneRoot.get(key));
+                Arrays.stream(value).forEach(predicate::value);
+                phonePredicate.add(predicate);
+            });
+            query.where(cb.and(phonePredicate.toArray(Predicate[]::new)));
+            return session.createQuery(query).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all phones with params " + params, e);
+        }
     }
 }
