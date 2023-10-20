a87aeb359d714b29b5f400cc929827868d55da13
Anatolii Nazarov <anatoly_n@ukr.net>
2023-10-20 16:43:53 +0300

    For check

--- src/test/java/ma/hibernate/dao/PhoneDaoImplTest.java
+++ src/test/java/ma/hibernate/dao/PhoneDaoImplTest.java
@@ -6,7 +6,6 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import ma.hibernate.model.Phone;
 import org.junit.Assert;
 import org.junit.Before;
