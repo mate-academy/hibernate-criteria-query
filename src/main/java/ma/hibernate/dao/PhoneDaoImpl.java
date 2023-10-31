428f3597110bd50ef4e66e32d790e0527283e714
Vlad Kucher <kuchervlad49@gmail.com>
2023-10-31 17:25:56 +0200

    added impl for PhoneDaoImpl

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,9 +1,17 @@
 package ma.hibernate.dao;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +20,51 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(phone);
+            transaction.commit();
+            return phone;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't create phone" + phone, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
-    @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        Session session = factory.openSession();
+        try {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> cq = cb.createQuery(Phone.class);
+            Root<Phone> phoneRoot = cq.from(Phone.class);
+
+            final Predicate[] finalPredicate = {cb.conjunction()}; // Use an array
+
+            params.entrySet().stream()
+                    .filter(entry -> entry.getValue() != null && entry.getValue().length > 0)
+                    .forEach(entry -> {
+                        String paramKey = entry.getKey();
+                        String[] paramValues = entry.getValue();
+                        Expression<String> paramExpression = phoneRoot.get(paramKey);
+                        Predicate paramPredicate = paramExpression.in((Object[]) paramValues);
+                        finalPredicate[0] = cb.and(finalPredicate[0], paramPredicate);
+                    });
+
+            cq.where(finalPredicate[0]);
+            Query<Phone> query = session.createQuery(cq);
+
+            return query.getResultList();
+        } finally {
+            session.close();
+        }
     }
 }
