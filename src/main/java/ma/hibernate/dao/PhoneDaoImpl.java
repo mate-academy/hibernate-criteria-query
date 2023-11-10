2a03355a825f3b8e9b8ea15065109e161b3af085
Dima Semeniuk <semeniuk.dima@ukr.net>
2023-11-10 15:11:18 +0200

    implemented findAll method using CriteriaApi

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,9 +1,15 @@
 package ma.hibernate.dao;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +18,46 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(phone);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save phone to the DB", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return phone;
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> query = builder.createQuery(Phone.class);
+            Root<Phone> phoneRoot = query.from(Phone.class);
+
+            Predicate combinePredicate = builder.conjunction();
+            for (Map.Entry<String, String[]> paramValue : params.entrySet()) {
+                CriteriaBuilder.In<Object> predicate = builder
+                        .in(phoneRoot.get(paramValue.getKey()));
+                for (Object value : paramValue.getValue()) {
+                    predicate.value(value);
+                }
+                combinePredicate = builder.and(combinePredicate, predicate);
+            }
+
+            query.where(combinePredicate);
+            return session.createQuery(query).getResultList();
+        }
     }
 }
+
