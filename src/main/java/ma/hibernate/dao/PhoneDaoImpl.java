6b6f2810d8780448ef26d5a72d96541490a20938
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2023-11-20 16:56:35 +0200

    implemented solution

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,14 +1,13 @@
 package ma.hibernate.dao;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Path;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Handler;
-
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
@@ -16,6 +15,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
+
     public PhoneDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
@@ -26,7 +26,7 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
         Transaction transaction = null;
         try {
             session = factory.openSession();
-            transaction = session.getTransaction();
+            transaction = session.beginTransaction();
             session.persist(phone);
             transaction.commit();
         } catch (HibernateException e) {
@@ -45,56 +45,29 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
         try (Session session = factory.openSession()) {
-            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-            CriteriaQuery<Phone> criteriaQuery = criteriaBuilder.createQuery(Phone.class);
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> criteriaQuery = cb.createQuery(Phone.class);
             Root<Phone> root = criteriaQuery.from(Phone.class);
-
-            Predicate[] predicates = buildPredicates(params, criteriaBuilder, root);
-            criteriaQuery.where(predicates);
-
+            List<Predicate> predicates = buildPredicates(params, cb, root);
+            criteriaQuery.where(predicates.toArray(new Predicate[0]));
             return session.createQuery(criteriaQuery).getResultList();
         } catch (HibernateException e) {
             throw new RuntimeException("Failed to retrieve phones from db", e);
         }
     }
 
-    private Predicate[] buildPredicates(Map<String, String[]> params, CriteriaBuilder criteriaBuilder, Root<Phone> root) {
+    private List<Predicate> buildPredicates(Map<String, String[]> params,
+                                            CriteriaBuilder cb, Root<Phone> root) {
         List<Predicate> predicateList = new ArrayList<>();
-
         for (Map.Entry<String, String[]> entry : params.entrySet()) {
             String parameter = entry.getKey();
-            String[] values = entry.getValue();
-
-            switch (parameter) {
-                case "color":
-                    CriteriaBuilder.In<String> colorInClause = criteriaBuilder.in(root.get("color"));
-                    for (String color : values) {
-                        colorInClause.value(color);
-                    }
-                    predicateList.add(colorInClause);
-                    break;
-
-                case "producer":
-                    CriteriaBuilder.In<String> producerInClause = criteriaBuilder.in(root.get("maker"));
-                    for (String producer : values) {
-                        producerInClause.value(producer);
-                    }
-                    predicateList.add(producerInClause);
-                    break;
-
-                case "countryManufactured":
-                    CriteriaBuilder.In<String> countryManufacturedInClause = criteriaBuilder.in(root.get("countryManufactured"));
-                    for (String country : values) {
-                        countryManufacturedInClause.value(country);
-                    }
-                    predicateList.add(countryManufacturedInClause);
-                    break;
+            Path<String> objectPath = root.get(parameter);
+            CriteriaBuilder.In<String> inPredicate = cb.in(objectPath);
+            for (String value : entry.getValue()) {
+                inPredicate.value(value);
             }
+            predicateList.add(inPredicate);
         }
-
-        return predicateList.toArray(new Predicate[0]);
+        return predicateList;
     }
-
-
-
 }
