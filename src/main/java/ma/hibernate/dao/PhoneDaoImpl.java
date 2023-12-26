bf0fa5dcb3202364d5f9a155794be6cc7e3cd1e8
Maksym Holubitskyi <golub1718@gmail.com>
2023-12-26 19:42:39 +0200

    implemented all PhoneDao methods:  - implemented create() method  - implemented findAll() method

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,9 +1,16 @@
 package ma.hibernate.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+import org.hibernate.query.criteria.JpaCriteriaQuery;
+import org.hibernate.query.criteria.JpaInPredicate;
+import org.hibernate.query.criteria.JpaRoot;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +19,43 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(phone);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't add phone to DB: " + phone, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return phone;
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            HibernateCriteriaBuilder cb = session.getCriteriaBuilder();
+            JpaCriteriaQuery<Phone> criteriaQuery = cb.createQuery(Phone.class);
+            JpaRoot<Phone> phoneRoot = criteriaQuery.from(Phone.class);
+            List<JpaInPredicate<Object>> predicateList = new ArrayList<>(params.size());
+            for (Map.Entry<String, String[]> entry : params.entrySet()) {
+                JpaInPredicate<Object> somePredicate = cb.in(phoneRoot.get(entry.getKey()));
+                for (String value : entry.getValue()) {
+                    somePredicate.value(value);
+                }
+                predicateList.add(somePredicate);
+            }
+            JpaInPredicate<Phone>[] predicatesArray = predicateList.toArray(new JpaInPredicate[0]);
+            criteriaQuery.where(predicatesArray);
+            return session.createQuery(criteriaQuery).getResultList();
+        }
     }
 }
