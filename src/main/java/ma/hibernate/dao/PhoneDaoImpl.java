b3e44f0d0db150bd05241340b9d97a376851b639
TkachukTara <zaz42988@gmail.com>
2024-01-09 12:59:45 +0200

    Done HW

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,8 +1,13 @@
 package ma.hibernate.dao;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
@@ -12,11 +17,31 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        try (Session session = factory.openSession()) {
+            session.save(phone);
+            return phone;
+        }
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> criteriaQuery = criteriaBuilder.createQuery(Phone.class);
+            Root<Phone> phoneRoot = criteriaQuery.from(Phone.class);
+            List<CriteriaBuilder.In<String>> predicates = new ArrayList<>();
+            for (Map.Entry<String, String[]> entry : params.entrySet()) {
+                CriteriaBuilder.In<String> predicate = criteriaBuilder
+                        .in(phoneRoot.get(entry.getKey()));
+                for (String value : entry.getValue()) {
+                    predicate.value(value);
+                }
+                predicates.add(predicate);
+            }
+            CriteriaBuilder.In<String>[] predicatesArray
+                    = predicates.toArray(new CriteriaBuilder.In[0]);
+            criteriaQuery.where(criteriaBuilder.and(predicatesArray));
+            return session.createQuery(criteriaQuery).getResultList();
+        }
     }
 }
