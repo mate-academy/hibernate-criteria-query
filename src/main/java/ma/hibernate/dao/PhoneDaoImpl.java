3054cb8a8879bdd362576b54ffc1c823b3640a90
Andrii Laver <laver.andriy@gmail.com>
2024-01-22 12:43:18 +0200

    implemented PhoneDao methods

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,9 +1,15 @@
 package ma.hibernate.dao;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +18,39 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(phone);
+            transaction.commit();
+            return phone;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert phone " + phone, e);
+        }
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        Session session = factory.openSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
+        Root<Phone> root = query.from(Phone.class);
+
+        Predicate resultPredicate = cb.and();
+        for (Map.Entry<String, String[]> entry : params.entrySet()) {
+            CriteriaBuilder.In<Object> tempPredicate = cb.in(root.get(entry.getKey()));
+            for (String s : entry.getValue()) {
+                tempPredicate.value(s);
+            }
+            resultPredicate = cb.and(tempPredicate, resultPredicate);
+        }
+
+        query.where(resultPredicate);
+        return session.createQuery(query).getResultList();
     }
 }
