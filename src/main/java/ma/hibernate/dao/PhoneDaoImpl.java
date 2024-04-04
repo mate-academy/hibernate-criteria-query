fc7ae5798c119087f3d18162080aa05f09bbcd47
Valentyn Sharshon <woosay333@gmail.com>
2024-04-04 16:38:23 +0300

    implemented create and findAll methods

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,9 +1,17 @@
 package ma.hibernate.dao;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Path;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import ma.hibernate.model.Phone;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +20,44 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Transaction transaction = null;
+        try (var session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(phone);
+            transaction.commit();
+            return phone;
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException(String.format(
+                    "Can`t add a phone %s to the DB", phone
+            ), ex);
+        }
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (var session = factory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
+            Root<Phone> phoneRoot = query.from(Phone.class);
+            Set<Predicate> predicates = new HashSet<>();
+
+            for (Map.Entry<String, String[]> entry : params.entrySet()) {
+                String param = entry.getKey();
+                Path<String> paramPath = phoneRoot.get(param);
+                CriteriaBuilder.In<String> inPredicate = cb.in(paramPath);
+                for (String value : entry.getValue()) {
+                    inPredicate.value(value);
+                }
+                predicates.add(inPredicate);
+            }
+
+            query.where(predicates.toArray(new Predicate[0]));
+            return session.createQuery(query).getResultList();
+        } catch (Exception ex) {
+            throw new RuntimeException("Can`t get phones from DB with provided parameters");
+        }
     }
 }
