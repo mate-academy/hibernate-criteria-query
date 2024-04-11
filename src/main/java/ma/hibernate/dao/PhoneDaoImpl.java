46457399fb4de59f82340131bcfe2f98dd9f5668
DmitryLazyuka <83352819+DmitryLazyuka@users.noreply.github.com>
2024-04-11 14:43:30 +0300

    Resolved hibernate-criteria-query Task

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,8 +1,14 @@
 package ma.hibernate.dao;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
@@ -12,11 +18,35 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            session.persist(phone);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't add phone to the DB " + phone, e);
+        }
+        return phone;
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
+            Root<Phone> rootPhone = query.from(Phone.class);
+            List<Predicate> whereConditions = new ArrayList<>();
+            for (Map.Entry<String, String[]> entry : params.entrySet()) {
+                CriteriaBuilder.In<String> inCondition
+                        = cb.in(rootPhone.get(entry.getKey()));
+                for (String option : entry.getValue()) {
+                    inCondition.value(option);
+                }
+                whereConditions.add(inCondition);
+            }
+            query.where(cb.and(whereConditions.toArray(Predicate[]::new)));
+            return session.createQuery(query).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find phones", e);
+        }
     }
 }
