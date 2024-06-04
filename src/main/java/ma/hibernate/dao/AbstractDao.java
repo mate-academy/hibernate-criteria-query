940051aca2d11561d1fbd786bc850c6c35d1835e
Oleksii Bashkirov <bashkirov.o.u@ukr.net>
2024-06-04 16:17:32 +0300

    Finished the implementation of the PhoneDao class; Modified the AbstractDao class; created the hibernate.cfg.xml file;

--- src/main/java/ma/hibernate/dao/AbstractDao.java
+++ src/main/java/ma/hibernate/dao/AbstractDao.java
@@ -1,11 +1,35 @@
 package ma.hibernate.dao;
 
+import java.util.function.Function;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public abstract class AbstractDao {
     protected final SessionFactory factory;
 
     protected AbstractDao(SessionFactory sessionFactory) {
         this.factory = sessionFactory;
+    }
+
+    protected <T> T executeTransaction(Function<Session, T> function) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            T result = function.apply(session);
+            transaction.commit();
+            return result;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Transaction failed: ", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
