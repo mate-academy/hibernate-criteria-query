c8890731adafe644f81bc21dc11a807a4f05686e
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-07-11 10:43:05 +0300

    PhoneDaoImpl.java

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -37,12 +37,12 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
             var root = cq.from(Phone.class);
 
             List<JpaPredicate> predicates = params.entrySet().stream()
-                .flatMap(entry -> {
-                    String key = entry.getKey();
-                    String[] values = entry.getValue();
-                    return Arrays.stream(values).map(value -> cb.equal(root.get(key), value));
-                })
-                .collect(Collectors.toList());
+                    .flatMap(entry -> {
+                        String key = entry.getKey();
+                        String[] values = entry.getValue();
+                        return Arrays.stream(values).map(value -> cb.equal(root.get(key), value));
+                    })
+                    .collect(Collectors.toList());
 
             if (!predicates.isEmpty()) {
                 cq.where(cb.or(predicates.toArray(new JpaPredicate[0])));
