924709649090ebc71ec5d19c33c569c0dc1777a6
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-07-11 07:42:10 +0300

    PhoneDaoImpl.java

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -3,14 +3,13 @@ package ma.hibernate.dao;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import ma.hibernate.model.Phone;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+import org.hibernate.query.criteria.JpaCriteriaQuery;
+import org.hibernate.query.criteria.JpaPredicate;
+import ma.hibernate.model.Phone;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -32,17 +31,17 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
         try (Session session = factory.openSession()) {
-            CriteriaBuilder cb = session.getCriteriaBuilder();
-            CriteriaQuery<Phone> cq = cb.createQuery(Phone.class);
-            Root<Phone> root = cq.from(Phone.class);
+            HibernateCriteriaBuilder cb = session.getCriteriaBuilder();
+            JpaCriteriaQuery<Phone> cq = cb.createQuery(Phone.class);
+            var root = cq.from(Phone.class);
 
-            Predicate[] predicates = params.entrySet().stream()
+            JpaPredicate[] predicates = params.entrySet().stream()
                 .flatMap(entry -> {
                     String key = entry.getKey();
                     String[] values = entry.getValue();
                     return Arrays.stream(values).map(value -> cb.equal(root.get(key), value));
                 })
-                .toArray(Predicate[]::new);
+                .toArray(JpaPredicate[]::new);
 
             cq.where(cb.or(predicates));
             Query<Phone> query = session.createQuery(cq);
