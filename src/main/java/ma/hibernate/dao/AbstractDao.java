78551098a984fa61fc9f6a128c3ae194c1c5dc50
mgucma <marek.gucm@gmail.com>
2024-07-23 12:11:36 +0200

    criteria query

--- src/main/java/ma/hibernate/dao/AbstractDao.java
+++ src/main/java/ma/hibernate/dao/AbstractDao.java
@@ -1,11 +1,29 @@
 package ma.hibernate.dao;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-public abstract class AbstractDao {
+public abstract class AbstractDao<T> {
     protected final SessionFactory factory;
 
     protected AbstractDao(SessionFactory sessionFactory) {
         this.factory = sessionFactory;
+    }
+
+    protected Session getCurrentSession() {
+        return factory.getCurrentSession();
+    }
+
+    public T save(T entity) {
+        getCurrentSession().saveOrUpdate(entity);
+        return entity;
+    }
+
+    public void delete(T entity) {
+        getCurrentSession().delete(entity);
+    }
+
+    public T findById(Class<T> clazz, Long id) {
+        return getCurrentSession().get(clazz, id);
     }
 }
