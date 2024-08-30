abb404b88d3665b03c6c08dbbffa20c848a56be8
foxstudent106239 <mykhailo.kornukh@gmail.com>
2024-08-30 16:49:59 +0300

    last fixes

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,11 +1,12 @@
 package ma.hibernate.dao;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
 import ma.hibernate.model.Phone;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -44,16 +45,17 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
             CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
             CriteriaQuery<Phone> phoneCriteriaQuery = criteriaBuilder.createQuery(Phone.class);
             Root<Phone> phoneRoot = phoneCriteriaQuery.from(Phone.class);
-            CriteriaBuilder.In<String> paramPredicate = null;
+            CriteriaBuilder.In<String> paramPredicate;
+            List<CriteriaBuilder.In> predicates = new ArrayList<>();
             for (var entry: params.entrySet()) {
                 paramPredicate = criteriaBuilder.in(
                         phoneRoot.get(entry.getKey()));
                 for (String paramValue: entry.getValue()) {
                     paramPredicate.value(paramValue);
                 }
-                phoneCriteriaQuery.where(criteriaBuilder.and(paramPredicate));
-
+                predicates.add(paramPredicate);
             }
+            phoneCriteriaQuery.select(phoneRoot).where(predicates.toArray(new Predicate[]{}));
             return session.createQuery(phoneCriteriaQuery).getResultList();
         }
     }
