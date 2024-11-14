330253d88db02506aafdba5921d626e9ded329fc
Oleksii Deineka <deineka219@gmail.com>
2024-11-14 21:05:53 +0200

    the task was written according to the requirements

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,8 +1,15 @@
 package ma.hibernate.dao;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Path;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
@@ -12,11 +19,31 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            session.save(phone);
+            session.getTransaction().commit();
+            return phone;
+        }
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
+
+            Root<Phone> phoneRoot = query.from(Phone.class);
+            List<Predicate> predicates = new ArrayList<>();
+
+            params.forEach((key, values) -> {
+                Path<String> attributePath = phoneRoot.get(key);
+                Predicate predicate = attributePath.in((Object[]) values);
+                predicates.add(predicate);
+            });
+            query.select(phoneRoot).where(predicates.toArray(new Predicate[0]));
+            return session.createQuery(query).getResultList();
+        }
     }
 }
