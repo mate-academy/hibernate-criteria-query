a8a41026a03b239c6a75909384c2ea2954173e44
Ihor <ihor.rome@gmail.com>
2024-11-26 04:23:07 +0200

    add solution

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -3,7 +3,14 @@ package ma.hibernate.dao;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+import org.hibernate.query.criteria.JpaCriteriaQuery;
+import org.hibernate.query.criteria.JpaInPredicate;
+import org.hibernate.query.criteria.JpaPredicate;
+import org.hibernate.query.criteria.JpaRoot;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +19,44 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(phone);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't add phone to DB. Phone: " + phone, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return phone;
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            HibernateCriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            JpaCriteriaQuery<Phone> query = criteriaBuilder.createQuery(Phone.class);
+            JpaRoot<Phone> root = query.from(Phone.class);
+            JpaPredicate predicate = criteriaBuilder.and();
+            for (Map.Entry<String, String[]> entry : params.entrySet()) {
+                JpaInPredicate<Object> inPredicate = criteriaBuilder.in(root.get(entry.getKey()));
+                for (String value : entry.getValue()) {
+                    inPredicate.value(value);
+                }
+                predicate = criteriaBuilder.and(predicate, inPredicate);
+            }
+            query.where(predicate);
+            return session.createQuery(query).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/ma/hibernate/model/Phone.java
+++ src/main/java/ma/hibernate/model/Phone.java
@@ -4,8 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "phone")
 public class Phone implements Cloneable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,54 +24,6 @@ public class Phone implements Cloneable {
     private String os;
     private String countryManufactured;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getMaker() {
-        return maker;
-    }
-
-    public void setMaker(String maker) {
-        this.maker = maker;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getOs() {
-        return os;
-    }
-
-    public void setOs(String os) {
-        this.os = os;
-    }
-
-    public String getCountryManufactured() {
-        return countryManufactured;
-    }
-
-    public void setCountryManufactured(String countryManufactured) {
-        this.countryManufactured = countryManufactured;
-    }
-
     @Override
     public Object clone() {
         try {
@@ -71,17 +31,5 @@ public class Phone implements Cloneable {
         } catch (CloneNotSupportedException e) {
             throw new RuntimeException("Can't make clone of " + this);
         }
-    }
-
-    @Override
-    public String toString() {
-        return "Phone{"
-            + "id=" + id
-            + ", model='" + model + '\''
-            + ", maker='" + maker + '\''
-            + ", color='" + color + '\''
-            + ", os='" + os + '\''
-            + ", countryManufactured='" + countryManufactured + '\''
-            + '}';
     }
 }
