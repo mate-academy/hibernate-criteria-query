0cf5cc720d746a53ec4531d68d18f018369f57eb
Ihor Tkachuk <iono19031@gmail.com>
2024-11-29 22:19:59 +0200

    added solution

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -5,11 +5,9 @@ import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
-import ma.hibernate.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -24,11 +22,10 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
         Transaction transaction = null;
         Session session = null;
         try {
-            session = HibernateUtil.getSessionFactory().openSession();
+            session = factory.openSession();
             transaction = session.beginTransaction();
             session.persist(phone);
             transaction.commit();
-            System.out.println("Phone created: " + phone);
             return phone;
         } catch (Exception e) {
             if (transaction != null) {
@@ -44,7 +41,7 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = factory.openSession()) {
             CriteriaBuilder builder = session.getCriteriaBuilder();
             CriteriaQuery<Phone> criteria = builder.createQuery(Phone.class);
             Root<Phone> root = criteria.from(Phone.class);
@@ -56,12 +53,8 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
                 String[] values = entry.getValue();
                 Predicate predicate = root.get(key).in((Object[]) values);
                 predicates.add(predicate);
-                System.out.println("Adding predicate for key: " + key + ", values: " + Arrays.toString(values));
             }
-
             criteria.where(predicates.toArray(new Predicate[0]));
-            List<Phone> result = session.createQuery(criteria).getResultList();
-            System.out.println("Query result: " + result);
             return session.createQuery(criteria).getResultList();
         } catch (Exception e) {
             throw new RuntimeException("Can't find phones by parameters", e);
