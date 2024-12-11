720400a92ee5538870cc5663dcfe4b1804d8f618
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-12-11 23:45:25 +0200

    Fix

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,12 +1,12 @@
 package ma.hibernate.dao;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import ma.hibernate.model.Phone;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -55,8 +55,8 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
             List<Predicate> predicates = new ArrayList<>();
 
             for (Map.Entry<String, String[]> entry : params.entrySet()) {
-                CriteriaBuilder.In<String> predicate = criteriaBuilder.
-                        in(phoneRoot.get(entry.getKey()));
+                CriteriaBuilder.In<String> predicate = criteriaBuilder
+                        .in(phoneRoot.get(entry.getKey()));
 
                 for (String value : entry.getValue()) {
                     predicate.value(value);
