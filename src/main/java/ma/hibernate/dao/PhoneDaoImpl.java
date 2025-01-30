27a1b7a0c7aebdb980ddf6de66c32954499a4400
oKukHtina <olakukhtina95@gmail.com>
2025-01-30 23:57:35 +0200

    implemented all PhoneDao methods

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,8 +1,15 @@
 package ma.hibernate.dao;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
@@ -12,11 +19,31 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            session.persist(phone);
+            session.getTransaction().commit();
+            return phone;
+        } catch (Exception e) {
+            throw new RuntimeException("Can't create phone : " + phone, e);
+        }
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> criteriaQuery = cb.createQuery(Phone.class);
+            Root<Phone> phoneRoot = criteriaQuery.from(Phone.class);
+            List<Predicate> listOfPredicates = new ArrayList<>();
+            for (Map.Entry<String, String[]> param : params.entrySet()) {
+                String key = param.getKey();
+                String[] value = param.getValue();
+                listOfPredicates.add(phoneRoot.get(key).in(Arrays.stream(value).toList()));
+            }
+
+            criteriaQuery.where(cb.and(listOfPredicates.toArray(new Predicate[0])));
+            return session.createQuery(criteriaQuery).getResultList();
+        }
     }
 }
