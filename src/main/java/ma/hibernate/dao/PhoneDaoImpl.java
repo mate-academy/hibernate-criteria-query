2a3da54624f87fbd5d1ee15983b511bdec58790e
FroGitHub <andrey8102310@gmail.com>
2025-02-09 19:06:16 +0200

    implement methods for PhoneDaoImpl

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,9 +1,16 @@
 package ma.hibernate.dao;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +19,47 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+
+            session.save(phone);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can`t save phone: " + phone);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return phone;
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> criteriaQuery = cb.createQuery(Phone.class);
+            Root<Phone> root = criteriaQuery.from(Phone.class);
+
+            List<CriteriaBuilder.In<String>> predicates = params.entrySet().stream()
+                    .map(entry -> {
+                        CriteriaBuilder.In<String> predicateKeyIn = cb.in(root.get(entry.getKey()));
+                        Arrays.stream(entry.getValue()).forEach(predicateKeyIn::value);
+                        return predicateKeyIn;
+                    })
+                    .toList();
+
+            criteriaQuery.where(cb.and(predicates.toArray(new Predicate[0])));
+
+            return session.createQuery(criteriaQuery).getResultList();
+        }
     }
+
 }
