60e2f7902e1465ad872cd529cd1d43f973d39e43
Bohdan Lysachenko <lysachenko111@ukr.net>
2025-02-13 12:21:17 +0200

    Solution

---
+++ src/main/java/ma/hibernate/exceptions/DataProcessingException.java
@@ -0,0 +1,7 @@
+package ma.hibernate.exceptions;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
