b09c2ade0da5d58b0627e3d8b37cd8f0c46e866d
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2025-03-26 11:01:43 +0100

    Опис змін

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,9 +1,14 @@
 package ma.hibernate.dao;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +17,41 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(phone);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Can not add phone to the database");
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return phone;
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> criteriaQuery = cb.createQuery(Phone.class);
+            Root<Phone> root = criteriaQuery.from(Phone.class);
+            CriteriaBuilder.In<String> predicateColor = cb.in(root.get("color"));
+            for (Map.Entry<String, String[]> entry : params.entrySet()) {
+                predicateColor.value(entry.getKey());
+            }
+
+            CriteriaBuilder.In<String[]> predicateModel = cb.in(root.get("maker"));
+            for (Map.Entry<String, String[]> entry : params.entrySet()) {
+                predicateModel.value(entry.getValue());
+            }
+
+            criteriaQuery.where(cb.and(predicateColor, predicateModel));
+            return session.createQuery(criteriaQuery).getResultList();
+        }
     }
 }
