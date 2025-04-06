76dc7ad620fa9e6cdf492cb2f0cf9b868def872a
d1kenpiken <skvyra.anton@gmail.com>
2025-04-06 19:29:28 +0200

    created solution

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -45,29 +45,29 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
             CriteriaBuilder cb = session.getCriteriaBuilder();
             CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
             Root<Phone> phoneRoot = query.from(Phone.class);
+
             List<Predicate> predicates = new ArrayList<>();
 
-            if (params.containsKey("countryManufactured")) {
-                String[] countries = params.get("countryManufactured");
-                if (countries.length > 0) {
-                    predicates.add(phoneRoot.get("countryManufactured").in((Object[]) countries));
-                }
+            String[] countries = params.get("countryManufactured");
+            if (countries != null && countries.length > 0) {
+                predicates.add(phoneRoot.get("countryManufactured").in((Object[]) countries));
             }
 
-            if (params.containsKey("countryManufactured")) {
-                String[] makers = params.get("maker");
-                if (makers.length > 0) {
-                    predicates.add(phoneRoot.get("maker").in((Object[]) makers));
-                }
+            String[] makers = params.get("maker");
+            if (makers != null && makers.length > 0) {
+                predicates.add(phoneRoot.get("maker").in((Object[]) makers));
             }
 
-            if (params.containsKey("color")) {
-                String[] colors = params.get("color");
-                if (colors.length > 0) {
-                    predicates.add(phoneRoot.get("color").in((Object[]) colors));
-                }
+            String[] colors = params.get("color");
+            if (colors != null && colors.length > 0) {
+                predicates.add(phoneRoot.get("color").in((Object[]) colors));
             }
-            query.select(phoneRoot).where(cb.and(predicates.toArray(new Predicate[0])));
+
+            query.select(phoneRoot);
+            if (!predicates.isEmpty()) {
+                query.where(cb.and(predicates.toArray(new Predicate[0])));
+            }
+
             return session.createQuery(query).getResultList();
         }
     }
